e12d1230db958d61a2ef73c2ef2507d68fee0eb6
Spax <spiritaxolotl@gmail.com>
2023-09-06 11:37:11 -0600

    initial commit moment

---
+++ .vscode/docs/mirin/placeholder.lua
@@ -0,0 +1,14 @@
+---@meta _
+---@diagnostic disable: duplicate-doc-field
+---@diagnostic disable: undefined-doc-name
+---@diagnostic disable: undefined-doc-param
+---@diagnostic disable: luadoc-miss-module-name
+---@diagnostic disable: missing-fields
+---@diagnostic disable: assign-type-mismatch
+
+
+---==================================================================================================================---
+---  PLACEHOLDER                                                                                                     ---
+---==================================================================================================================---
+
+-- add definemod, setdefault, set, ease, add, acc, reset, alias, get, aux, node, func, func_ease, perframe, etc
---
+++ .vscode/docs/notitg/ActorScroller.lua
@@ -0,0 +1,31 @@
+---@meta _
+---@diagnostic disable: duplicate-doc-field
+---@diagnostic disable: undefined-doc-name
+---@diagnostic disable: undefined-doc-param
+
+
+---==================================================================================================================---
+---  ACTORSCROLLER                                                                                                   ---
+---==================================================================================================================---
+
+--- @class ActorScroller: ActorFrame
+--- @field public __index table Gives you the ``ActorScroller`` table again
+local ActorScroller = {}
+
+--- Scrolls to the item at index ``index``, and makes it the currently selected item
+---
+--- |since_itg|
+---
+--- @param index float The index of the targetted item
+---
+--- @return void
+function ActorScroller:SetCurrentAndDestinationItem(index) end
+
+--- Returns an ``ActorScroller (MemoryAddress)`` string
+---
+--- |since_unk|
+---
+--- @return string
+function ActorScroller:__tostring() end
+
+return ActorScroller
